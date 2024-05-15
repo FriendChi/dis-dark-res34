25a79044e2066e1400af813e2b66d7573f5a4c37
FriendChi <137796057+FriendChi@users.noreply.github.com>
2024-05-15 19:02:31 +0800

    更新 train.py

--- train.py
+++ train.py
@@ -14,10 +14,9 @@ from evaluation.eval_wrapper import eval_lane
 python train.py 配置文件的路径 --log_path 保存日志和模型的路径
 '''
 #用来在一个数据加载器上进行一轮训练，计算损失函数，更新优化器，记录日志和指标等
-def train(net,teacher_net, data_loader, loss_dict, optimizer, scheduler,logger, epoch, metric_dict, dataset):
+def train(net, data_loader, loss_dict, optimizer, scheduler,logger, epoch, metric_dict, dataset):
     #将网络设置成训练模式
     net.train()
-    teacher_net.eval()
     #创建进度条
     progress_bar = dist_tqdm(train_loader)
     #r循环遍历进度条对象中的每个元素，每个元素都是一个包含了数据和标签（data and label）的元组（tuple）
@@ -26,7 +25,7 @@ def train(net,teacher_net, data_loader, loss_dict, optimizer, scheduler,logger, 
         global_step = epoch * len(data_loader) + b_idx
         #返回的是预测得到的数据
         #dataset是名称
-        results = inference(net, teacher_net,data_label, dataset)
+        results = inference(net, data_label, dataset)
         #计算损失值
         loss = calc_loss(loss_dict, results, logger, global_step, epoch)
         #将优化器中的梯度缓存清零，防止累计梯度
@@ -39,22 +38,22 @@ def train(net,teacher_net, data_loader, loss_dict, optimizer, scheduler,logger, 
         scheduler.step(global_step)
 
 
-        # if global_step % 20 == 0:
-        #     reset_metrics(metric_dict)
-        #     #update_metrics(metric_dict, results)
-        #     for me_name, me_op in zip(metric_dict['name'], metric_dict['op']):
-        #         logger.add_scalar('metric/' + me_name, me_op.get(), global_step=global_step)
-        #     logger.add_scalar('meta/lr', optimizer.param_groups[0]['lr'], global_step=global_step)
+        if global_step % 20 == 0:
+            reset_metrics(metric_dict)
+            update_metrics(metric_dict, results)
+            for me_name, me_op in zip(metric_dict['name'], metric_dict['op']):
+                logger.add_scalar('metric/' + me_name, me_op.get(), global_step=global_step)
+            logger.add_scalar('meta/lr', optimizer.param_groups[0]['lr'], global_step=global_step)
 
-        #     if hasattr(progress_bar,'set_postfix'):
-        #         kwargs = {me_name: '%.3f' % me_op.get() for me_name, me_op in zip(metric_dict['name'], metric_dict['op'])}
-        #         new_kwargs = {}
-        #         for k,v in kwargs.items():
-        #             if 'lane' in k:
-        #                 continue
-        #             new_kwargs[k] = v
-        #         progress_bar.set_postfix(loss = '%.3f' % float(loss), 
-        #                                 **new_kwargs)
+            if hasattr(progress_bar,'set_postfix'):
+                kwargs = {me_name: '%.3f' % me_op.get() for me_name, me_op in zip(metric_dict['name'], metric_dict['op'])}
+                new_kwargs = {}
+                for k,v in kwargs.items():
+                    if 'lane' in k:
+                        continue
+                    new_kwargs[k] = v
+                progress_bar.set_postfix(loss = '%.3f' % float(loss), 
+                                        **new_kwargs)
         
 #用来设置一些参数，初始化模型，数据加载器，优化器，学习率调度器，日志器等，并根据是否分布式训练进行相应的配置。
 # 如果有指定 finetune 或 resume 的模型路径，就从中加载模型权重。然后对每个 epoch 进行训练和评估，并保存最佳的模型。
@@ -99,27 +98,12 @@ if __name__ == "__main__":
     train_loader = get_train_loader(cfg)
     #根据cfg对象中的参数生成一个模型对象，并赋值
     net = get_model(cfg)
-    from model.model_tusimple import parsingNet1
-    teacher_net = parsingNet1(pretrained = True, backbone='34', num_grid_row = 100, 
-                             num_cls_row = 56, num_grid_col = 100, 
-                             num_cls_col = 41, num_lane_on_row = 4, 
-                             num_lane_on_col = 4, use_aux = False, 
-                             input_height = 320, input_width = 800, 
-                             fc_norm = False).cuda()
-
-
-    #加载训练过的模型参数
-    #读取保存好的预训练模型状态字典，并将其存储在resume_dict变量中
-    resume_dict = torch.load('/kaggle/input/res34-tu/Ultra-Fast-Lane-Detection-v2/log/20240510_112515_lr_5e-02_b_32/model_best.pth', map_location='cpu')
-    #将恢复的状态字典中的模型参数加载到目标模型
-    teacher_net.load_state_dict(resume_dict['model'])    
 
     if distributed:
         net = torch.nn.parallel.DistributedDataParallel(net, device_ids = [args.local_rank])
     optimizer = get_optimizer(net, cfg)
 
     #判断cfg中是否有finetune这个参数，如果有，就表示要从一个预训练的模型开始微调，就执行下面的代码块
-    #None
     if cfg.finetune is not None:
         dist_print('finetune from ', cfg.finetune)
         state_all = torch.load(cfg.finetune)['model']
@@ -129,19 +113,14 @@ if __name__ == "__main__":
                 state_clip[k] = v
         net.load_state_dict(state_clip, strict=False)
     #判断cfg中是否有resume这个参数，如果有，就表示要从一个之前保存的模型继续训练，就执行下面的代码块
-    #None
     if cfg.resume is not None:
-        #输出日志信息
         dist_print('==> Resume model from ' + cfg.resume)
-        #读取保存好的预训练模型状态字典，并将其存储在resume_dict变量中
         resume_dict = torch.load(cfg.resume, map_location='cpu')
-        #将恢复的状态字典中的模型参数加载到目标模型
         net.load_state_dict(resume_dict['model'])
-        #在每次恢复训练时，还需要将优化器之前保存的状态恢复回来
         if 'optimizer' in resume_dict.keys():
             optimizer.load_state_dict(resume_dict['optimizer'])
-        #将resume_epoch变量赋值为之前训练的轮数+1
-        resume_epoch = int(os.path.split(cfg.resume)[1][2:5]) + 1
+        #resume_epoch = int(os.path.split(cfg.resume)[1][2:5]) + 1
+        resume_epoch = 90
     else:
         resume_epoch = 0
 #创建一个学习率调度器，用来动态地调整学习率
@@ -161,13 +140,13 @@ if __name__ == "__main__":
     res = None
     for epoch in range(resume_epoch, cfg.epoch):
         
-        train(net, teacher_net,train_loader, loss_dict, optimizer, scheduler,logger, epoch, metric_dict, cfg.dataset)
+        train(net, train_loader, loss_dict, optimizer, scheduler,logger, epoch, metric_dict, cfg.dataset)
         #调用训练数据加载器的reset()方法，重置加载器的状态，以便下一轮训练使用。
         train_loader.reset()
         #对神经网络模型进行评估，传入网络模型net、配置信息cfg、当前epoch值epoch以及日志记录器logger。
         #评估结果将保存在变量res中
         res = eval_lane(net, cfg, ep = epoch, logger = logger)
-
+        print("epoch:",epoch)
         if res is not None and res > max_res:
             #若评估结果更好，则将其赋值给max_res，以便跟踪记录最优结果。
             max_res = res
